9605374825292206b925f47576540f2935cc4b14
Mariano Giuffrida <marianogiuffrida@hotmail.it>
2015-09-06 16:40:49 +1000

    Make option menu invisible for now.

--- app/src/main/java/com/marianogiuffrida/pilotcalc/UI/MainActivity.java
+++ app/src/main/java/com/marianogiuffrida/pilotcalc/UI/MainActivity.java
@@ -107,11 +107,11 @@ public class MainActivity extends ActionBarActivity
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -119,27 +119,28 @@ public class MainActivity extends ActionBarActivity
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        // Handle action bar actions click
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
+//        // Handle action bar actions click
+//        switch (item.getItemId()) {
+//            case R.id.action_settings:
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
     }
 
-    /***
-     * Called when invalidateOptionsMenu() is triggered
-     */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // if nav drawer is opened, hide the action items
-        if (mDrawerLayout != null) {
-            boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-            menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
+//    /***
+//     * Called when invalidateOptionsMenu() is triggered
+//     */
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // if nav drawer is opened, hide the action items
+//        if (mDrawerLayout != null) {
+//            boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+//            menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+//        }
+//        return super.onPrepareOptionsMenu(menu);
+//    }
 
     @Override
     public void setTitle(CharSequence title) {
